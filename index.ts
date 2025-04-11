b7062a3289094ad7229d0036aabb6963eb6e3cbf
AlanJuker <alan4630@gmail.com>
2025-04-10 20:29:42 -0500

    feat(logger): implement logs with LowDB

--- index.ts
+++ index.ts
@@ -7,9 +7,13 @@ const entries = await fetchEntries();
 // Check the CLI argument
 const isShort = process.argv[2] === 'short';
 
-//Call the respective filter based on the argument chose
+// Call the respective filter based on the argument chose
 const filtered = isShort
   ? filter5OrLessWords(entries)
   : filterMoreThan5Words(entries);
 
+// Save logs
+await logUsage(isShort ? '≤5_words' : '>5_words', filtered.length);
+
+// Print results
 console.log(filtered);
