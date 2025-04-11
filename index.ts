7d64bfd78edf5ca12abd1ca7af2488e8a910b029
AlanJuker <alan4630@gmail.com>
2025-04-10 19:50:34 -0500

    feat: implement CLI argument for filtering entries by title length

--- index.ts
+++ index.ts
@@ -3,3 +3,13 @@ import { filterMoreThan5Words, filter5OrLessWords } from './src/filters';
 import { logUsage } from './src/logger';
 
 const entries = await fetchEntries();
+
+// Check the CLI argument
+const isShort = process.argv[2] === 'short';
+
+//Call the respective filter based on the argument chose
+const filtered = isShort
+  ? filter5OrLessWords(entries)
+  : filterMoreThan5Words(entries);
+
+console.log(filtered);
